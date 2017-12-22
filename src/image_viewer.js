376491cdee2976eb0e33e30575f6dc497ef755fb
Anthony Torres <atorres@g2crowd.com>
2017-12-22 13:42:01 -0600

    First commit

---
+++ src/image_viewer.js
@@ -0,0 +1,14 @@
+import big from '../assets/big.jpg'
+import small from '../assets/small.jpg'
+import '../styles/image_viewer.css'
+
+const image = document.createElement('img');
+image.src = small;
+
+document.body.appendChild(image);
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
+
